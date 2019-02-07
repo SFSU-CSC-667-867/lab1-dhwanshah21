cb37e7e51401e5b72cce8726464c33a8a3bd70e1
Saloni Shah <saloni.shah281@gmail.com>
2019-02-06 21:12:54 -0800

    commit all

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,5 @@
 setImmediate(() => console.log('Now'));
 process.nextTick(() => console.log('Hello'));
-
+// next event on the queue
+// finishes first and then gives the priority to the next tick
 console.log('world');
